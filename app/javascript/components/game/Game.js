411d4032a528bfe0062e63a5ec28df65ea28c7cc
czarjulius <juliusczar.jc@gmail.com>
2020-04-15 10:14:44 +0200

    added react logo to the landing page

---
+++ app/javascript/components/game/Game.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Game extends Component{
+    render(){
+        return(
+            <div className="container">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/1/18/React_Native_Logo.png" className="img-rounded" alt="Cinque Terre"/>
+            </div>
+        )
+    }
+}
+
+export default Game
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -4,23 +4,13 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import 'bootstrap/dist/css/bootstrap.css'
+import Game from'../components/game/Game'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Game />,
     document.body.appendChild(document.createElement('div')),
   )
 })
