4a97c08b0d550cb641b1d90204280cbbbbee6431
czarjulius <juliusczar.jc@gmail.com>
2020-05-22 16:46:31 +0200

    updated rake version

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,6 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-  config.hosts << "julius-tictactoe.herokuapp.com"
   # Show full error reports.
   config.consider_all_requests_local = true
 
