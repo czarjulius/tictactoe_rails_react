0444a87b41cbfca60f4983918770307b6740d886
czarjulius <juliusczar.jc@gmail.com>
2020-06-08 08:55:18 +0200

    pull develop on remote

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,12 +1,14 @@
  #!/bin/bash
  
 echo 'Starting to Deploy...'
-ssh -i "tictactoeaws.pem" ubuntu@54.202.2.231 "
-    cd tictactoe_rails_react && git pull origin develop -f 
-    docker-compose down && docker-compose up -d
-    docker exec -it postgres bash && su - postgres && create database tictactoe\; && \q 
-    docker-compose run web bundle exec rake db:migrate
-    "
+ssh -i "tictactoeaws.pem" ubuntu@54.202.2.231 "cd tictactoe_rails_react 
+        sudo docker-compose down
+        git pull origin develop -f 
+        "
+#     docker-compose down && docker-compose up -d
+#     docker exec -it postgres bash && su - postgres && create database tictactoe\; && \q 
+#     docker-compose run web bundle exec rake db:migrate
+#     "
 
 
 
@@ -16,12 +18,13 @@ ssh -i "tictactoeaws.pem" ubuntu@54.202.2.231 "
 
 
 
-# ssh -i "tictactoeaws.pem" ubuntu@54.202.2.231 "cd tictactoe_rails_react && sudo docker-compose up -d"
 #  git pull origin develop -f \
-#  docker-compose down \
-# docker exec -it postgres bash && su - postgres && create database tictactoe\; && \q
-# docker-compose run web bundle exec rake db:migrate
-# docker-compose up
-# docker-compose build
-# docker-compose run web bundle exec rake db:create && docker-compose run web bundle exec rake db:migrate && docker-compose up
-
+# ssh -i "tictactoeaws.pem" ubuntu@54.202.2.231 "cd tictactoe_rails_react &&  docker-compose up"
+#     docker-compose down
+#     docker-compose up -d
+#     docker exec -it postgres bash && su - postgres && create database tictactoe\; && \q
+#     docker-compose run web bundle exec rake db:migrate
+#     docker-compose up
+#     docker-compose build
+#     docker-compose run web bundle exec rake db:create && docker-compose run web bundle exec rake db:migrate
+# "
