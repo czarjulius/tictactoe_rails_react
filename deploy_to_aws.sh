6666f268b594076109e4f9a67afcff8f0cef2214
czarjulius <juliusczar.jc@gmail.com>
2020-06-08 17:58:37 +0200

    added delay script

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,30 +1,8 @@
  #!/bin/bash
  
 echo 'Starting to Deploy...'
-ssh -i "tictactoeaws.pem" ubuntu@54.202.2.231 "cd tictactoe_rails_react 
-        sudo docker-compose down
-        git pull origin develop -f 
+ssh -i "tictactoeaws.pem" ubuntu@52.38.40.75 "cd tictactoe_rails_react 
+        sudo docker-compose down && cd ..
+        sudo rm -rf tictactoe_rails_react && git clone https://github.com/czarjulius/tictactoe_rails_react.git && cd tictactoe_rails_react && echo 'You are doing well'
+        sudo docker-compose build && sudo docker-compose up -d
         "
-#     docker-compose down && docker-compose up -d
-#     docker exec -it postgres bash && su - postgres && create database tictactoe\; && \q 
-#     docker-compose run web bundle exec rake db:migrate
-#     "
-
-
-
-
-
-
-
-
-
-#  git pull origin develop -f \
-# ssh -i "tictactoeaws.pem" ubuntu@54.202.2.231 "cd tictactoe_rails_react &&  docker-compose up"
-#     docker-compose down
-#     docker-compose up -d
-#     docker exec -it postgres bash && su - postgres && create database tictactoe\; && \q
-#     docker-compose run web bundle exec rake db:migrate
-#     docker-compose up
-#     docker-compose build
-#     docker-compose run web bundle exec rake db:create && docker-compose run web bundle exec rake db:migrate
-# "
