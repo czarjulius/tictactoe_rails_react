a0698a9ea6d5977c2fb0d339a15561f40db2a07d
czarjulius <juliusczar.jc@gmail.com>
2020-06-09 18:53:41 +0200

    trigger deployment with CI and esc fingerprint

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
  
 echo 'Starting to Deploy...'
-ssh -i "tictactoeaws.pem" ubuntu@52.38.40.75 "cd tictactoe_rails_react 
+ssh -i "tictactoeaws.pem" -o StrictHostKeyChecking=no ubuntu@52.38.40.75 "cd tictactoe_rails_react 
         sudo docker-compose down && cd ..
         sudo rm -rf tictactoe_rails_react && sudo docker image prune -f
         git clone https://github.com/czarjulius/tictactoe_rails_react.git && cd tictactoe_rails_react && echo 'You are doing well'
