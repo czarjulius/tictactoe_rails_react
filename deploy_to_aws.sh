0994182733fdd3e663cc34a41f6c6efd9e2bea6b
czarjulius <juliusczar.jc@gmail.com>
2020-06-10 10:38:56 +0200

    update the repo on docker contater

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,10 +1,11 @@
  #!/bin/bash
  
 echo 'Starting to Deploy...'
-ssh -i "tictactoeaws.pem" -o StrictHostKeyChecking=no ubuntu@52.38.40.75 "cd tictactoe_rails_react 
-        sudo docker-compose down && cd ..
-        sudo rm -rf tictactoe_rails_react && sudo docker image prune -f
-        git clone https://github.com/czarjulius/tictactoe_rails_react.git && cd tictactoe_rails_react && echo 'You are doing well'
+ssh -i "tictactoeaws.pem" -o StrictHostKeyChecking=no ubuntu@52.38.40.75 " sudo docker image prune -f 
+        cd tictactoe_rails_react 
+        sudo docker-compose down
+        git fetch origin
+        git reset --hard origin/develop  &&  echo 'You are doing well'
         sudo docker-compose build && sudo docker-compose up -d
         "
 echo 'Deployment completed successfully'
